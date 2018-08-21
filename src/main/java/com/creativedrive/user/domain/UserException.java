bb3f7cca7008d842daab63ede0a2b08f91f3d6f5
cadu.goncalves <cadu.goncalves@gmail.com>
2018-08-21 16:28:45 -0300

    - Added missing exception

---
+++ src/main/java/com/creativedrive/user/domain/UserException.java
@@ -0,0 +1,11 @@
+package com.creativedrive.user.domain;
+
+/**
+ * User exception
+ */
+public class UserException extends RuntimeException {
+
+    public UserException(String message) {
+        super(message);
+    }
+}
